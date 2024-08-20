433dce904b00c59492f6825f6e4efb967baa6e77
QQcodee <castawebdesign@gmail.com>
2024-08-20 14:05:56 -0600

    root mod

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,67 +6,67 @@ const Header = () => {
   const { cart, cartCount } = useCart();
   const navigate = useNavigate();
   return (
-    <header
-      style={{
-        backgroundColor: "black",
-
-        width: "100vw",
-
-        height: "60px",
-
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        color: "white",
-        fontSize: "20px",
-        paddingTop: "10px",
-        paddingBottom: "10px",
-        paddingLeft: "20px",
-        paddingRight: "20px",
-
-        position: "sticky",
-        top: "0",
-
-        zIndex: "10",
-      }}
-    >
-      <i style={{ cursor: "pointer" }} className="material-icons">
-        menu
-      </i>
-      <img
+    <header>
+      <div
         style={{
+          backgroundColor: "black",
+
           height: "60px",
-          width: "60px",
-          objectFit: "contain",
-          borderRadius: "50%",
-          cursor: "pointer",
+
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          color: "white",
+          fontSize: "20px",
+          paddingTop: "10px",
+          paddingBottom: "10px",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+
+          position: "sticky",
+          top: "0",
+
+          zIndex: "10",
         }}
-        onClick={() => navigate("/")}
-        src={Logo}
-        alt="Logo"
-      />
-
-      <div style={{ cursor: "pointer" }} onClick={() => navigate("/carrito")}>
-        {" "}
-        <i className="material-icons">shopping_bag</i>
-        <sub
+      >
+        <i style={{ cursor: "pointer" }} className="material-icons">
+          menu
+        </i>
+        <img
           style={{
-            fontWeight: "bold",
-            color: "white",
-            backgroundColor: "red",
+            height: "60px",
+            width: "60px",
+            objectFit: "contain",
             borderRadius: "50%",
-            paddingLeft: "5px",
-            paddingRight: "5px",
-            fontFamily: "Poppins",
-            position: "relative",
-            top: "-15px",
-            left: "-5px",
-
-            fontSize: "10px",
+            cursor: "pointer",
           }}
-        >
-          {cartCount && cartCount}
-        </sub>
+          onClick={() => navigate("/")}
+          src={Logo}
+          alt="Logo"
+        />
+
+        <div style={{ cursor: "pointer" }} onClick={() => navigate("/carrito")}>
+          {" "}
+          <i className="material-icons">shopping_bag</i>
+          <sub
+            style={{
+              fontWeight: "bold",
+              color: "white",
+              backgroundColor: "red",
+              borderRadius: "50%",
+              paddingLeft: "5px",
+              paddingRight: "5px",
+              fontFamily: "Poppins",
+              position: "relative",
+              top: "-15px",
+              left: "-5px",
+
+              fontSize: "10px",
+            }}
+          >
+            {cartCount && cartCount}
+          </sub>
+        </div>
       </div>
     </header>
   );
